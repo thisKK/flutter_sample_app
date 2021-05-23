9301c39343bfc0318e88acd2974b94d77163498d
thisKK <sonicrat12@gmail.com>
2021-05-23 15:21:17 +0700

    add unit test setLanguageSelected

--- test/screens/login/login_controller_test.dart
+++ test/screens/login/login_controller_test.dart
@@ -3,15 +3,28 @@ import 'package:sample_flutter/enums/language.dart';
 import 'package:sample_flutter/screens/login/login_controller.dart';
 
 main() {
-  test("should retrun flag th", () {
+  group("getImagePathLanguage", () {
     var controller = LoginController();
-    var actual = controller.getImagePathLanguage(Language.th);
-    expect(actual, "assets/thai-flag.png");
+    test("should retrun flag th", () {
+      var actual = controller.getImagePathLanguage(Language.th);
+      expect(actual, "assets/thai-flag.png");
+    });
+
+    test("should retrun flag en", () {
+      var actual = controller.getImagePathLanguage(Language.en);
+      expect(actual, "assets/eng-flag.png");
+    });
   });
 
-  test("should retrun flag en", () {
+  group("setLanguageSelected", () {
     var controller = LoginController();
-    var actual = controller.getImagePathLanguage(Language.en);
-    expect(actual, "assets/eng-flag.png");
+    test("should language selected as th", () {
+      controller.setLanguageSelected(Language.th);
+      expect(controller.languageSelected.value, Language.th);
+    });
+    test("should language selected as en", () {
+      controller.setLanguageSelected(Language.en);
+      expect(controller.languageSelected.value, Language.en);
+    });
   });
 }
